c97f195656c2f18be0a5981436f2c5f662114846
Petros <psdeme@hotmail.com>
2023-04-17 20:58:40 +0200

    added results

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import category_encoders as ce
-from keras.layers import Input
+import tensorflow as tf
+from keras.layers import Input, InputLayer
 from keras.layers import Dense
 from keras.layers import BatchNormalization
 from keras.models import Sequential
@@ -9,6 +10,9 @@ from keras import Model
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import tensorflow_probability as tfp
+from ctgan import CTGAN
+from sklearn.impute import SimpleImputer
 
 df = pd.read_json('./recipes_processed.json')
 
@@ -120,6 +124,11 @@ df.loc[df['hop 2 phase'].str.startswith('First Wort'), 'hop 2 phase'] = 'First W
 df.loc[df['hop 3 phase'].str.startswith('First Wort'), 'hop 3 phase'] = 'First Wort'
 df.loc[df['hop 4 phase'].str.startswith('First Wort'), 'hop 4 phase'] = 'First Wort'
 
+df.loc[df['hop 1 phase'].str.startswith('Dry Hop'), 'hop 1 phase'] = 'Dry Hop'
+df.loc[df['hop 2 phase'].str.startswith('Dry Hop'), 'hop 2 phase'] = 'Dry Hop'
+df.loc[df['hop 3 phase'].str.startswith('Dry Hop'), 'hop 3 phase'] = 'Dry Hop'
+df.loc[df['hop 4 phase'].str.startswith('Dry Hop'), 'hop 4 phase'] = 'Dry Hop'
+
 df['hop 1 time'] = df['hop 1 time'].apply(lambda x: x[:-4].strip())
 df['hop 2 time'] = df['hop 2 time'].apply(lambda x: x[:-4].strip())
 df['hop 3 time'] = df['hop 3 time'].apply(lambda x: x[:-4].strip())
@@ -149,131 +158,58 @@ df['fermentable 5 name'] = df['fermentables'].apply(lambda x: fermentable_name(x
 
 df = df.drop(['hops','fermentables','method'], axis = 1)
 
-encoder = ce.CountEncoder()
-df['hop 1 name_transformed'] = encoder.fit_transform(df['hop 1 name'])
-df['hop 2 name_transformed'] = encoder.fit_transform(df['hop 2 name'])
-df['hop 3 name_transformed'] = encoder.fit_transform(df['hop 3 name'])
-df['hop 4 name_transformed'] = encoder.fit_transform(df['hop 4 name'])
+discrete_columns = [
+    'hop 1 form',
+    'hop 2 form',
+    'hop 3 form',
+    'hop 4 form',
+    'hop 1 phase',
+    'hop 2 phase',
+    'hop 3 phase',
+    'hop 4 phase',
+    'hop 1 name',
+    'hop 2 name',
+    'hop 3 name',
+    'hop 4 name',
+    'fermentable 1 name',
+    'fermentable 2 name',
+    'fermentable 3 name',
+    'fermentable 4 name',
+    'fermentable 5 name',
+    'yeast'
+]
+df['batch'] = df['batch'].astype(float)
 
-df['hop 1 phase_transformed'] = encoder.fit_transform(df['hop 1 phase'])
-df['hop 2 phase_transformed'] = encoder.fit_transform(df['hop 2 phase'])
-df['hop 3 phase_transformed'] = encoder.fit_transform(df['hop 3 phase'])
-df['hop 4 phase_transformed'] = encoder.fit_transform(df['hop 4 phase'])
+imp = SimpleImputer(strategy='most_frequent', missing_values='-', keep_empty_features=True)
+cols = df.columns
 
-df['hop 1 form_transformed'] = encoder.fit_transform(df['hop 1 form'])
-df['hop 2 form_transformed'] = encoder.fit_transform(df['hop 2 form'])
-df['hop 3 form_transformed'] = encoder.fit_transform(df['hop 3 form'])
-df['hop 4 form_transformed'] = encoder.fit_transform(df['hop 4 form'])
+imputed = imp.fit_transform(df)
 
-df['fermentable 1 name_transformed'] = encoder.fit_transform(df['fermentable 1 name'])
-df['fermentable 2 name_transformed'] = encoder.fit_transform(df['fermentable 2 name'])
-df['fermentable 3 name_transformed'] = encoder.fit_transform(df['fermentable 3 name'])
-df['fermentable 4 name_transformed'] = encoder.fit_transform(df['fermentable 4 name'])
-df['fermentable 5 name_transformed'] = encoder.fit_transform(df['fermentable 5 name'])
+df = pd.DataFrame(imputed, columns=cols)
+df['batch'] = df['batch'].astype(float)
 
-df['yeast_transformed'] = encoder.fit_transform(df['yeast'])
+df['fermentable 1 weight'] = df['fermentable 1 weight'].astype(float)
+df['fermentable 2 weight'] = df['fermentable 2 weight'].astype(float)
+df['fermentable 3 weight'] = df['fermentable 3 weight'].astype(float)
+df['fermentable 4 weight'] = df['fermentable 4 weight'].astype(float)
+df['fermentable 5 weight'] = df['fermentable 5 weight'].astype(float)
 
-yeast_trans = df[['yeast_transformed', 'yeast']].drop_duplicates()
+df['hop 1 weight'] = df['hop 1 weight'].astype(float)
+df['hop 2 weight'] = df['hop 2 weight'].astype(float)
+df['hop 3 weight'] = df['hop 3 weight'].astype(float)
+df['hop 4 weight'] = df['hop 4 weight'].astype(float)
 
-hop_trans_1 = df[['hop 1 name_transformed', 'hop 1 name']].drop_duplicates()
-hop_trans_2 = df[['hop 2 name_transformed', 'hop 2 name']].drop_duplicates()
-hop_trans_3 = df[['hop 3 name_transformed', 'hop 3 name']].drop_duplicates()
-hop_trans_4 = df[['hop 4 name_transformed', 'hop 4 name']].drop_duplicates()
+df['hop 1 time'] = df['hop 1 time'].astype(float)
+df['hop 2 time'] = df['hop 2 time'].astype(float)
+df['hop 3 time'] = df['hop 3 time'].astype(float)
+df['hop 4 time'] = df['hop 4 time'].astype(float)
 
-hop_trans_1_phase = df[['hop 1 phase_transformed', 'hop 1 phase']].drop_duplicates()
-hop_trans_2_phase = df[['hop 2 phase_transformed', 'hop 2 phase']].drop_duplicates()
-hop_trans_3_phase = df[['hop 3 phase_transformed', 'hop 3 phase']].drop_duplicates()
-hop_trans_4_phase = df[['hop 4 phase_transformed', 'hop 4 phase']].drop_duplicates()
+df['yeast'] = df['yeast'].astype(str)
 
-hop_trans_1_form = df[['hop 1 form_transformed', 'hop 1 form']].drop_duplicates()
-hop_trans_2_form = df[['hop 2 form_transformed', 'hop 2 form']].drop_duplicates()
-hop_trans_3_form = df[['hop 3 form_transformed', 'hop 3 form']].drop_duplicates()
-hop_trans_4_form = df[['hop 4 form_transformed', 'hop 4 form']].drop_duplicates()
+ctgan = CTGAN(epochs=40, cuda=True, verbose=True)
+ctgan.fit(df, discrete_columns)
 
-fermentable_trans_1 = df[['fermentable 1 name_transformed', 'fermentable 1 name']].drop_duplicates()
-fermentable_trans_2 = df[['fermentable 2 name_transformed', 'fermentable 2 name']].drop_duplicates()
-fermentable_trans_3 = df[['fermentable 3 name_transformed', 'fermentable 3 name']].drop_duplicates()
-fermentable_trans_4 = df[['fermentable 4 name_transformed', 'fermentable 4 name']].drop_duplicates()
-fermentable_trans_5 = df[['fermentable 5 name_transformed', 'fermentable 5 name']].drop_duplicates()
-
-df['fermentable 1 name'] = df['fermentable 1 name_transformed']
-df['fermentable 2 name'] = df['fermentable 2 name_transformed']
-df['fermentable 3 name'] = df['fermentable 3 name_transformed']
-df['fermentable 4 name'] = df['fermentable 4 name_transformed']
-df['fermentable 5 name'] = df['fermentable 5 name_transformed']
-
-df['hop 1 name'] = df['hop 1 name_transformed']
-df['hop 2 name'] = df['hop 2 name_transformed']
-df['hop 3 name'] = df['hop 3 name_transformed']
-df['hop 4 name'] = df['hop 4 name_transformed']
-
-df['hop 1 phase'] = df['hop 1 phase_transformed']
-df['hop 2 phase'] = df['hop 2 phase_transformed']
-df['hop 3 phase'] = df['hop 3 phase_transformed']
-df['hop 4 phase'] = df['hop 4 phase_transformed']
-
-df['hop 1 form'] = df['hop 1 form_transformed']
-df['hop 2 form'] = df['hop 2 form_transformed']
-df['hop 3 form'] = df['hop 3 form_transformed']
-df['hop 4 form'] = df['hop 4 form_transformed']
-
-df['yeast'] = df['yeast_transformed']
-
-df = df.drop(['yeast_transformed', 'fermentable 1 name_transformed', 'fermentable 2 name_transformed', 'fermentable 3 name_transformed', 'fermentable 4 name_transformed', 'fermentable 5 name_transformed',
-'hop 1 name_transformed','hop 2 name_transformed', 'hop 3 name_transformed','hop 4 name_transformed', 'hop 1 phase_transformed','hop 2 phase_transformed', 'hop 3 phase_transformed','hop 4 phase_transformed',
-'hop 1 form_transformed', 'hop 2 form_transformed', 'hop 3 form_transformed', 'hop 4 form_transformed'], axis=1)
-
-print(df.columns)
-
-print(df.head())
-print(df.shape)
-
-latent_dimension = 4
-batch_size = 20
-hidden_nodes = 16
-
-X = np.asarray(df).astype(np.float32)
-
-# Encoder
-input_encoder = Input(shape=(32,), name="Input_Encoder")
-x = BatchNormalization()(input_encoder)
-x = Dense(hidden_nodes, activation="relu", name="Hidden_Encoding")(x)
-x = BatchNormalization()(x)
-z = Dense(latent_dimension, name="Mean")(x)
-encoder = Model(input_encoder, z)
-
-# Decoder
-input_decoder = Input(shape=(latent_dimension,), name="Input_Decoder")
-x = BatchNormalization()(input_decoder)
-x = Dense(hidden_nodes, activation="relu", name="Hidden_Decoding")(x)
-x = BatchNormalization()(x)
-decoder_output = Dense(32, activation="linear", name="Decoded")(x)
-decoder = Model(input_decoder, decoder_output, name="Decoder")
-
-encoder_decoder = decoder(encoder(input_encoder))
-vae = Model(input_encoder, encoder_decoder)
-
-vae.compile(loss="mean_squared_error", optimizer="adam")
-history = vae.fit(
-    X, X, shuffle=True, epochs=500, batch_size=20, validation_split=0.2, verbose=2
-).history
-vae.save('./trained_vae.h5')
-
-def plot_loss(history):
-    train_loss = history["loss"]
-    val_loss = history["val_loss"]
-
-    plt.figure(figsize=(16, 12))
-    plt.title("Mean squared error", fontname="Times New Roman Bold")
-    plt.plot(train_loss, label="Train", linewidth=3)
-    plt.plot(val_loss, label='Test', linewidth=3)
-    plt.xlabel("Epochs")
-
-    plt.legend()
-    plt.savefig("Loss.png", dpi=300)
-    plt.show()
-    print(f"Training MSE = {np.sqrt(train_loss[-1])}")
-    print(f"Validation MSE = {np.sqrt(val_loss[-1])}")
-
-
-plot_loss(history)
+# Create synthetic data
+synthetic_data = ctgan.sample(10)
+synthetic_data.to_csv('./synthetic_recipes_40_epochs.csv')
+print(synthetic_data)
